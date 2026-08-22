893ea3e1fa39566ab81d7ab9ea2d8d0ff40fd7ee
kiyo1204 <kyoumu6209@gmail.com>
2026-08-22 23:02:04 +0900

    画面作成(MVP)

---
+++ frontend/compornents/LabStatus.tsx
@@ -0,0 +1,13 @@
+const LabStatus = () => {
+    return (
+        <section className="lab-status-card">
+            <h2>研究室の状況</h2>
+
+            <p className="lab-status">
+                ゼミ中
+            </p>
+        </section>
+    )
+}
+
+export default LabStatus
